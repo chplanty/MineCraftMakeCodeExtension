f672db24cf8b643c18af863a0df884d82ffde982
xkoegler-m <113993434+xkoegler-m@users.noreply.github.com>
2022-09-21 11:41:37 +0200

    fix off-by-one errors in tower placement

--- main.ts
+++ main.ts
@@ -1,13 +1,3 @@
-/**
- * Fun with castles!
- * Build your castle walls and towers.
- * Functions are mapped to blocks using various macros
- * in comments starting with %. The most important macro
- * is "block", and it specifies that a block should be
- * generated for an **exported** function.
- */
-
-//% color="#AA278D" weight=100
 namespace castles {
     function drawRectangle(length: number, width: number, blockType: number) {
         for (let index = 0; index < 2; index++) {
@@ -25,7 +15,7 @@ namespace castles {
     function drawAlternatingRectangle(length: number, width: number, blockType: number) {
         function drawAlternatingLine(l: number, placeAtStart: boolean) {
             let putBlock = placeAtStart;
-            for (let index = 0; index < l - 1; index++) {
+            for (let index2 = 0; index2 < l - 1; index2++) {
                 builder.move(FORWARD, 1)
                 if (putBlock) {
                     builder.place(blockType)
@@ -36,10 +26,10 @@ namespace castles {
         }
 
         builder.place(blockType)
-        for (let index = 0; index < 2; index++) {
-            let putBlock = drawAlternatingLine(length, false)
+        for (let index3 = 0; index3 < 2; index3++) {
+            let putBlock2 = drawAlternatingLine(length, false)
             builder.turn(LEFT_TURN)
-            drawAlternatingLine(width, putBlock)
+            drawAlternatingLine(width, putBlock2)
             builder.turn(LEFT_TURN)
         }
     }
@@ -55,7 +45,7 @@ namespace castles {
         // builder is at the center of the tower - shift it to a corner
         builder.shift(-Math.floor(WallWidth / 2), 0, -Math.floor(WallWidth / 2))
         // build 2 walls from bottom to top (up to WallHeight)
-        for (let index = 0; index < WallHeight - 1; index++) {
+        for (let index4 = 0; index4 < WallHeight - 1; index4++) {
             drawRectangle(WallLenght, WallWidth, BlocType)
             builder.move(UP, 1)
         }
@@ -68,35 +58,21 @@ namespace castles {
 
     //% block
     export function buildCastleTower() {
-        let BlocType = 0
-        let WallWidth = 0
-        let WallLenght = 0
-        let WallHeight = 0
-        let alternate = false
-        let putBlock = false
+        let BlockType = 0
         let TowerSize = 0
         let TowerHeight = 0
 
-        BlocType = COBBLESTONE
+        BlockType = COBBLESTONE
         TowerSize = 5
         TowerHeight = 8
         // builder.teleportTo(pos(0, 0, 0))
         // builder is at the center of the tower - shift it to a corner
-        builder.shift((0 - TowerSize) / 2 + 1, 0, (0 - TowerSize) / 2 + 1)
+        builder.shift( - Math.floor(TowerSize / 2), 0, - Math.floor(TowerSize / 2))
         // let posX = Math.sin(3.14159265*2)  // will need this for circular tower's base
         // build 4 walls from bottom to top (up to TowerHeight)
         for (let height2 = 0; height2 <= TowerHeight; height2++) {
             // add 4 walls
-            for (let index = 0; index < 4; index++) {
-                // builder.mark()
-                // add a wall
-                for (let index = 0; index < TowerSize - 1; index++) {
-                    builder.place(BlocType)
-                    builder.move(FORWARD, 1)
-                }
-                // builder.tracePath(BlocType)
-                builder.turn(LEFT_TURN)
-            }
+            drawRectangle(TowerSize, TowerSize, BlockType);
             builder.move(UP, 1)
         }
         // build the upper part, larger by 1
@@ -104,26 +80,12 @@ namespace castles {
         builder.shift(-1, 0, -1)
         for (let height22 = 0; height22 <= 2; height22++) {
             // add 4 walls
-            for (let index = 0; index < 4; index++) {
-                // builder.mark()
-                // add a wall
-                for (let index42 = 0; index42 <= TowerSize; index42++) {
-                    if (height22 == 2 && index42 % 2 == 1) {
-
-                    } else {
-                        builder.place(BlocType)
-                    }
-                    builder.move(FORWARD, 1)
-                }
-                // builder.tracePath(BlocType)
-                builder.turn(LEFT_TURN)
-            }
+            drawRectangle(TowerSize + 2, TowerSize + 2, BlockType)
             builder.move(UP, 1)
         }
         // mov builder back to center
-        builder.shift(TowerSize / 2, 0 - (TowerHeight + 4), TowerSize / 2)
+        builder.shift(Math.floor(TowerSize / 2) + 1, 0 - (TowerHeight + 4), Math.floor(TowerSize / 2) + 1)
         // verify it's ok
         builder.place(GLASS)
     }
-
 }
